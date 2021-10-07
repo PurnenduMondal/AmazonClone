30bf7957afcd6c14f08c0e9cfd494666e01cd033
Purnendu Mondal <38310111+PurnenduMondal@users.noreply.github.com>
2021-10-08 03:13:19 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import "firebase/compat/firestore"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCutSVqJQoyMZzMEwAmo8gXcGHmiw-WCrY",
-  authDomain: "clone-513e7.firebaseapp.com",
-  projectId: "clone-513e7",
-  storageBucket: "clone-513e7.appspot.com",
-  messagingSenderId: "248801855402",
-  appId: "1:248801855402:web:dd26403948fb08db610dd3",
-  measurementId: "G-S7PNDZJ1VY"
+  apiKey: "AIzaSyCutSVqJQoyMZzMEwAfasdfmo8gXcGHmiw-WCrY",
+  authDomain: "clone-5dsf1afaf3e7.firebaseapp.com",
+  projectId: "clone-513fasdfe7",
+  storageBucket: "clone-afadf513e7.appspot.com",
+  messagingSenderId: "2488018asdff55402",
+  appId: "1:248801855402:web:dd26403948fb08db610ddfasasfd3",
+  measurementId: "hhjfjNDZfaasdfJ1VY"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
